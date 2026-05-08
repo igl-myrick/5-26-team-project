eccd26f17885ba4b42845f6954e39eb344363c75
unknown <igl.myrick@gmail.com>
2026-05-08 11:12:23 -0700

    add second test for getColors function

--- __tests__/getColors.test.js
+++ __tests__/getColors.test.js
@@ -7,4 +7,14 @@ describe("getColors", () => {
   test("should correctly return an object", () => {
     expect(typeof output).toEqual("object");
   });
+
+  test("should correctly return a group of rgb values", () => {
+    expect(output).toEqual([
+      'rgb(72, 27, 19)',
+      'rgb(172, 158, 77)',
+      'rgb(106, 110, 95)',
+      'rgb(52, 55, 40)',
+      'rgb(73, 20, 14)'
+    ]);
+  });
 });
