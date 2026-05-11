edadb95771a8e9498df367f942c225a7ea6fac51
MakeyeB <makeyebuchanan0617@gmail.com>
2026-05-11 10:14:01 -0700

    add callColorApi function to index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ColorService from './api.js'
+
+// Business Logic
+
+function callColorApi(resolveCallback, rejectCallback) {
+  ColorService.getColorList().then(resolveCallback, rejectCallback);
+}
 
 // UI Logic
 
