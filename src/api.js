368f85efe7bda1b86aa44285b853ace744c69aff
MakeyeB <makeyebuchanan0617@gmail.com>
2026-05-11 10:14:01 -0700

    add callColorApi function to index file

--- src/api.js
+++ src/api.js
@@ -1,14 +1,16 @@
-export default function() {
-  return new Promise(function(resolve, reject) {
-    const request = new XMLHttpRequest();
-    request.addEventListener("loadend", function() {
-      if (this.status === 200 && this.responseText[0] === "{") {
-        resolve(JSON.parse(this.responseText));
-      } else {
-        reject();
-      }
+export default class ColorService {
+  static getColorList() {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      request.addEventListener("loadend", function() {
+        if (this.status === 200 && this.responseText[0] === "{") {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject();
+        }
+      });
+      request.open("POST", "http://colormind.io/api/", true);
+      request.send(JSON.stringify({model: "default"}));
     });
-    request.open("POST", "http://colormind.io/api/", true);
-    request.send(JSON.stringify({model: "default"}));
-  });
+  }
 }
