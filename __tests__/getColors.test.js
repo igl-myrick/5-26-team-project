da695ac8518951099323305721f98d2ecd0d9925
unknown <igl.myrick@gmail.com>
2026-05-11 11:51:30 -0700

    update first test for getColors function

--- __tests__/getColors.test.js
+++ __tests__/getColors.test.js
@@ -4,8 +4,8 @@ describe("getColors", () => {
   const input = {"result":[[72,27,19],[172,158,77],[106,110,95],[52,55,40],[73,20,14]]};
   const output = getColors(input);
 
-  test("should correctly return an object", () => {
-    expect(typeof output).toEqual("object");
+  test("should correctly return an array", () => {
+    expect(Array.isArray(output)).toEqual(true);
   });
 
   test("should correctly return a group of rgb values", () => {
