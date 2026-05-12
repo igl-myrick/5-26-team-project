f5afcf79c46afbe40784d9f51a8a5dcd292af6e3
unknown <igl.myrick@gmail.com>
2026-05-12 10:06:00 -0700

    add second test for rgbToHex function

--- __tests__/rgbToHex.test.js
+++ __tests__/rgbToHex.test.js
@@ -1,9 +1,14 @@
 import rgbToHex from "../src/rgbToHex";
 
 describe("rgbToHex", () => {
-  const input = [[72,27,19],[172,158,77],[106,110,95],[52,55,40],[73,20,14]];
+  const input = {"result":[[72,27,19],[172,158,77],[106,110,95],[52,55,40],[73,20,14]]};
+  const output = rgbToHex(input);
   
   test("should correctly return an array", () => {
     expect(Array.isArray(rgbToHex(input))).toEqual(true);
+  });
+
+  test.each(output)("should correctly return an array of strings", (str) => {
+    expect(typeof str).toEqual("string");
   });
 });
