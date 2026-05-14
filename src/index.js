b5aa83facfbd467f40584019531e02146060f460
Josh Butcher <sjwb1999@gmail.com>
2026-05-14 11:13:27 -0700

    make changes to css / index.js / index.html.

--- src/index.js
+++ src/index.js
@@ -12,27 +12,42 @@ const disclaimerMenu = document.getElementById("disclaimer");
 const mainContentBody = document.getElementById("main-body");
 const disclaimerButton = document.getElementById("disclaimer-button-id");
 const displayButton = document.getElementById("display-button");
+const optionButton = document.getElementById("options");
+const cycleUp = document.getElementById("cycle-up");
+const cycleDown = document.getElementById("cycle-down");
+const resetPage = document.getElementById("reset-page");
 // Business Logic
 
 function grabColorList(model, resolveCallback, rejectCallback) {
   ColorService.getColorList(model).then(resolveCallback, rejectCallback);
 }
 
-// function grabApiModels(resolveCallback, rejectCallback) {
-//   ColorService.getApiModels().then(resolveCallback, rejectCallback);
-// }
+function grabApiModels(resolveCallback, rejectCallback) {
+  ColorService.getApiModels().then(resolveCallback, rejectCallback);
+}
+
+function handleModelSelection(response) {
+  const modelArr = response.result;
+  const rand = Math.floor(Math.random() * 6) + 1;
+  grabColorList(modelArr[rand], displayColors, printError);
+}
 
 // UI Logic
 
 function displayColors(response) {
   const output = getColors(response);
+  const hexValues = rgbToHex(response);
   const body7 = document.querySelector(".body7");
   const p = document.createElement("p");
   for (let i = 0; i < output.length; i++) {
-    const span = document.createElement("span");
-    span.style.color = output[i];
-    span.innerText = "aaaaa";
-    p.append(span); 
+    const palette = document.getElementById(`color${i+1}`);
+    palette.style.backgroundColor = `${output[i]}`;
+    palette.style.height = "40px";
+    palette.classList.add("bordered-frame");
+    const hexP = document.getElementById(`hex${i+1}`);
+    hexP.innerText = `${hexValues[i]}`;
+    hexP.style.color = `${output[i]}`;
+    
   }
   body7.append(p);
 }
@@ -41,15 +56,18 @@ function printError() {
   console.log("call failed");
 }
 
-function getData(response) {
-  return response.result;
-}
-
 displayButton.addEventListener("click", async function() {
-  return grabColorList("default", displayColors, printError);
+  return grabApiModels(handleModelSelection, printError);
 });
 
 disclaimerButton.addEventListener("click", function() {
   disclaimerMenu.classList.add("hidden");
   mainContentBody.classList.remove("hidden");
 });
+
+optionButton.addEventListener("click", function() {
+  const toggledMenu = document.getElementById("mainMenu");
+  console.log(toggledMenu.classList);
+  toggledMenu.classList.toggle("hidden");
+  
+})
