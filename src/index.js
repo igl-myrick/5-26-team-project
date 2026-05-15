270acbd288b0d3b3119f23b8b73a59dc70d1a1ca
Josh Butcher <sjwb1999@gmail.com>
2026-05-15 10:52:06 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const disclaimerMenu = document.getElementById("disclaimer");
 const mainContentBody = document.getElementById("main-body");
 const disclaimerButton = document.getElementById("disclaimer-button-id");
 const displayButton = document.getElementById("display-button");
-const cycleDown = document.getElementById("");
-const cycleUp = document.getElementById
+const optionButton = document.getElementById("options");
+const cycleUp = document.getElementById("cycle-up");
+const cycleDown = document.getElementById("cycle-down");
+const resetPage = document.getElementById("reset-page");
+
 // Business Logic
 
 function grabColorList(model, resolveCallback, rejectCallback) {
@@ -27,6 +30,7 @@ function grabApiModels(resolveCallback, rejectCallback) {
 function handleModelSelection(response) {
   const modelArr = response.result;
   const rand = Math.floor(Math.random() * 6) + 1;
+  grabColorList(modelArr[rand], handleArray, printError);
   grabColorList(modelArr[rand], displayColors, printError);
 }
 
@@ -41,15 +45,28 @@ function displayColors(response) {
     const palette = document.getElementById(`color${i+1}`);
     palette.style.backgroundColor = `${output[i]}`;
     palette.style.height = "40px";
+    palette.classList.add("bordered-frame");
     const hexP = document.getElementById(`hex${i+1}`);
     hexP.innerText = `${hexValues[i]}`;
     hexP.style.color = `${output[i]}`;
+    palette.addEventListener("click", () => handlePageChange(hexValues[i]));
   }
   body7.append(p);
 }
 
 function printError() {
   console.log("call failed");
+}
+
+function handleArray(response) {
+  const arrayList = response.result;
+  for (let i = 0; i < arrayList; i++) {
+    console.log(arrayList[i]);
+  }
+}
+
+function handlePageChange(color) {
+  console.log(color + "1")
 }
 
 displayButton.addEventListener("click", async function() {
@@ -60,3 +77,9 @@ disclaimerButton.addEventListener("click", function() {
   disclaimerMenu.classList.add("hidden");
   mainContentBody.classList.remove("hidden");
 });
+
+
+optionButton.addEventListener("click", function() {
+  const toggledMenu = document.getElementById("mainMenu");
+  toggledMenu.classList.toggle("hidden");
+})
